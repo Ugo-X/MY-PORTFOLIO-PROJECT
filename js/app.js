981619bf0464a8da3a6702c8e36641881eaa5324
Ugo-X <ugonnadike001@gmail.com>
2023-09-25 16:31:24 +0100

    update app.js

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ const closeBtn = document.querySelector("#close-btn");
 const sidebar = document.querySelector("#sidebar");
 const date = document.querySelector("#date");
 const btns = document.querySelectorAll('.more')
-const service = document.querySelector('.service')
+const services = document.querySelectorAll('.service')
 // add fixed class to navbar
 window.addEventListener("scroll", function () {
   if (window.pageYOffset > 80) {
@@ -24,11 +24,27 @@ closeBtn.addEventListener("click", function () {
 date.innerHTML = new Date().getFullYear();
 
 // show service text
-btns.forEach(function(btn){
-  btn.addEventListener('click', function(evt){
-    const service = evt.currentTarget.parentElement;
-    service.classList.toggle('show-text')
-  })
+// btns.forEach(function(btn){
+//   btn.addEventListener('click', function(evt){
+//     const service = evt.currentTarget.parentElement;
+//     service.classList.toggle('show-text')
+//   })
+// })
+// so the idea behind this is to loop over all the service articles and then by using the forEach method, we can decide what happens to each individual service article class
+// then once we have access to this individual item  amongst  our services, we can select an element without using the document. since the element we are looking for is within the service so we replace the document with service, since we are looking for the item within the service article.
+services.forEach(function(service){
+ const btn = service.querySelector('.more')
+ btn.addEventListener('click', function(){
+  // so when we click on this btn within the service article, the individual service should toggle the class show-text i.e remove the class show-text if it exists already or add it if it doesnt exist already.
+services.forEach(function(item){
+  if(item !== service){
+    item.classList.remove('show-text')
+  }
+})
+
+service.classList.toggle('show-text')
+
+ })
 })
 
 
